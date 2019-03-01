93adb3ca767818db64ba21b39c99a2ec3c5a45b7
andy <andy_viky@163.com>
2019-03-01 23:30:11 +0800

    complete search component

--- src/utils/wxRequest.js
+++ src/utils/wxRequest.js
@@ -1,32 +1,24 @@
 import wepy from 'wepy';
 import util from './util';
 import md5 from './md5';
-import tip from './tip'
 
 const API_SECRET_KEY = 'www.mall.cycle.com'
 const TIMESTAMP = util.getCurrentTime()
 const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase())
 
 const wxRequest = async (params = {}, url) => {
-    tip.loading();
+    global.tip.loading();
     let data = params.query || {};
     data.sign = SIGN;
     data.time = TIMESTAMP;
-    return new Promise((resolve, reject) => {
-      wepy.request({
-        url: url,
-        method: params.method || 'GET',
-        data: data,
-        header: { 'Content-Type': 'application/json' },
-      }).then(data => {
-        tip.loaded();
-        resolve(data);
-      }).catch(error => {
-        tip.loaded();
-        tip.error(error);
-        reject(error);
-      })
-    })
+    let res = wepy.request({
+      url: url,
+      method: params.method || 'GET',
+      data: data,
+      header: { 'Content-Type': 'application/json' },
+    });
+    global.tip.loaded();
+    return res;
 };
 
 
